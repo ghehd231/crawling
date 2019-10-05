34a4db4fbeb7f88b7d14b2d31539df8cc4cedd58
ghehd231 <ghehd231@naver.com>
2019-10-05 15:55:49 +0900

    Crawlling Product details - Image

--- frankenmono/franken_crawling.py
+++ frankenmono/franken_crawling.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-href_arr = []
-
 def getList(page):
     print(page)
     response = requests.get("http://www.frankenmono.com/product/shop_list.asp?page="+page+"&top_val=store&shop_chk=ALL&search_value=&shop_chk2=")
@@ -19,6 +17,10 @@ def getList(page):
 
     if len(result2) > 0:
         getList(str(page_plus))
+    else:
+        print('-------end Crawlling-------')
+
+
 # end getList()
 
 def getDetail(href):
@@ -26,25 +28,26 @@ def getDetail(href):
     result = res.text
 
     soup = BeautifulSoup(result, "html.parser")
-    #table 전체 가져오기
+    #상품 정보 table 전체 가져오기
     table = soup.select('#detail_right > div > table tr')
 
     #--------코드 가져오기----------
-
-    code = table[1].text
-    #strip() => str 변수의 모든 공백 제거
-    code = code.strip()
-
-    #code 기준으로 잘라서 리스트로 반환
-    code = code.split('code')
-
-    #split 으로 잘라온 후 code[~]으로 불러올때의 타입은 str이다
-    code = code[1].strip()
-    print(code)
+    #
+    # code = table[1].text
+    # #strip() => str 변수의 모든 공백 제거
+    # code = code.strip()
+    #
+    # #code 기준으로 잘라서 리스트로 반환
+    # code = code.split('code')
+    #
+    # #split 으로 잘라온 후 code[~]으로 불러올때의 타입은 str이다
+    # code = code[1].strip()
+    # print(code)
 
     # #--------이미지 가져오기------------
-    # image = soup.select('#detail_left img')
-    # print(image[0])
+    image = soup.select('#detail_left img')
+    first_image = image[0].get('src')
+    print(first_image)
 
 # end getDetail
 
